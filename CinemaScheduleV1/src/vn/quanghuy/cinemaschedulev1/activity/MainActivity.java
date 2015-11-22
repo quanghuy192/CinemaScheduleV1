c5cd1e5b59edc152390ff515196ddd6ebc48a495
yeuembubu <quanghuydang1902@gmail.com>
2015-11-22 13:11:26 +0700

    commit

--- CinemaScheduleV1/src/vn/quanghuy/cinemaschedulev1/activity/MainActivity.java
+++ CinemaScheduleV1/src/vn/quanghuy/cinemaschedulev1/activity/MainActivity.java
@@ -1,13 +1,34 @@
 package vn.quanghuy.cinemaschedulev1.activity;
 
-import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+	private FragmentTabHost fragmentTabHost;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		fragmentTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+		fragmentTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+
+		View newMovieTab = createTab(this, R.layout.tab_bg, R.drawable.new_movie_32);
+
 	}
+
+	private View createTab(Context context, int idLayout, int idImage) {
+		View view = LayoutInflater.from(context).inflate(idLayout, null);
+		ImageView imageView = (ImageView) view.findViewById(R.id.tabsImage);
+		imageView.setImageResource(idImage);
+		return view;
+	}
+
 }
---
+++ CinemaScheduleV1/src/vn/quanghuy/cinemaschedulev1/activity/NewMoieFragment.java
@@ -0,0 +1,18 @@
+package vn.quanghuy.cinemaschedulev1.activity;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewMoieFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View view = inflater.inflate(R.layout.new_movie_layout, null);
+		return view;
+	}
+
+}
