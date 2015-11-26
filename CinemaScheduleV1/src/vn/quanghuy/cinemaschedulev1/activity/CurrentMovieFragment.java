c146a8ab66ce40972216f36bcc01066d274e273c
yeuembubu <quanghuydang1902@gmail.com>
2015-11-26 19:02:53 +0700

    commit

--- CinemaScheduleV1/src/vn/quanghuy/cinemaschedulev1/activity/CurrentMovieFragment.java
+++ CinemaScheduleV1/src/vn/quanghuy/cinemaschedulev1/activity/CurrentMovieFragment.java
@@ -8,17 +8,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import vn.quanghuy.cinemaschedulev1.adapter.MovieAdapter;
 import vn.quanghuy.cinemaschedulev1.bean.Movie;
 import vn.quanghuy.cinemaschedulev1.utilities.HtmlParser;
 
 public class CurrentMovieFragment extends Fragment {
 
 	private List<Movie> movieList;
+	private ListView lvCurrent;
+	private ArrayAdapter<Movie> adapter;
+	private ProgressBar progressBar;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		View view = inflater.inflate(R.layout.current_movie_layout, null);
+		lvCurrent = (ListView) view.findViewById(R.id.lvCurrent);
+		progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
 		new CinemaAsynTask().execute();
 		return view;
 	}
@@ -26,11 +35,28 @@ public class CurrentMovieFragment extends Fragment {
 	class CinemaAsynTask extends AsyncTask<Void, Void, Void> {
 
 		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			progressBar.setVisibility(View.VISIBLE);
+		}
+
+		@Override
 		protected Void doInBackground(Void... params) {
 			// TODO Auto-generated method stub
 			HtmlParser htmlParser = new HtmlParser();
 			movieList = htmlParser.getMovieList();
+
 			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			progressBar.setVisibility(View.GONE);
+			adapter = new MovieAdapter(getActivity(), R.layout.item_cinema, movieList);
+			lvCurrent.setAdapter(adapter);
 		}
 
 	}
--- CinemaScheduleV1/src/vn/quanghuy/cinemaschedulev1/adapter/MovieAdapter.java
+++ CinemaScheduleV1/src/vn/quanghuy/cinemaschedulev1/adapter/MovieAdapter.java
@@ -3,22 +3,64 @@ package vn.quanghuy.cinemaschedulev1.adapter;
 import java.util.List;
 
 import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import vn.quanghuy.cinemaschedulev1.activity.R;
 import vn.quanghuy.cinemaschedulev1.bean.Movie;
 
 public class MovieAdapter extends ArrayAdapter<Movie> {
+	private List<Movie> movieList;
+	private Context context;
+	private int resource;
 
 	public MovieAdapter(Context context, int resource, List<Movie> objects) {
 		super(context, resource, objects);
 		// TODO Auto-generated constructor stub
+		this.movieList = objects;
+		this.context = context;
+		this.resource = resource;
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		return super.getView(position, convertView, parent);
+		
+		// sign view = convertView
+		// if convertView == null => create new view
+		
+		View view = convertView;
+		ViewHolder holder;
+		if (convertView == null) {
+			view = View.inflate(context, resource, null);
+			holder = new ViewHolder();
+			holder.imageView = (ImageView) view.findViewById(R.id.itemIcon);
+			holder.title = (TextView) view.findViewById(R.id.itemTitle);
+			holder.type = (TextView) view.findViewById(R.id.itemType);
+			
+			// set tag to save ViewHolder
+			view.setTag(holder);
+		} else {
+			
+			// if view exist => get ViewHolder to sign 
+			holder = (ViewHolder) view.getTag();
+		}
+
+		holder.imageView.setImageBitmap(movieList.get(position).getIcon());
+		holder.title.setText(movieList.get(position).getTitle());
+		holder.type.setText(movieList.get(position).getType());
+		return view;
+	}
+
+	// ViewHolder pattern
+	private class ViewHolder {
+		ImageView imageView;
+		TextView title;
+		TextView type;
 	}
 
 }
